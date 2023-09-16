963835d22b322f9581b5591559c2916069bc3f89
Marc Gulliver <Marc.gulliver100@gmail.com>
2023-09-16 14:21:28 +0100

    feature: add feature cards to the individual address pages.

--- resources/views/view-address.blade.php
+++ resources/views/view-address.blade.php
@@ -8,69 +8,65 @@
             </div>
         </div>
         <div class="grid-cols-1 sm:grid md:grid-cols-2 ">
-            <div class="mx-12 mt-6 flex flex-col self-start rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 sm:shrink-0 sm:grow sm:basis-0">
-                <div class="p-6">
-                    <a href="#!">
-                        <i class="fa-solid fa-file-invoice-dollar fa-2xl rounded-t-lg mt-6" style="font-size: 60px;"></i>
-                    </a>
-                    <h5 class="mb-2 mt-6 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
-                        Card title
-                    </h5>
-                    <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-                        This is a longer card with supporting text below as a natural
-                        lead-in to additional content. This content is a little bit
-                        longer.
-                    </p>
-                </div>
+            <div class="mx-12 mt-6 flex flex-col self-start rounded-lg bg-white rounded-lg shadow-2xl dark:bg-neutral-700 sm:shrink-0 sm:grow sm:basis-0">
+                <a href="#!">
+                    <div class="p-6">
+                        <i class="fa-solid fa-calculator fa-2xl rounded-t-lg mt-6 text-cyan-500" style="font-size: 60px;""></i>
+                        <h5 class="mb-2 mt-6 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+                            Tax Returns
+                        </h5>
+                        <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+                            Here you can upload, store, and view your tax returns and payments.
+                        </p>
+                    </div>
+                </a>
             </div>
-            <div class="mx-12 mt-6 flex flex-col self-start rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 sm:shrink-0 sm:grow sm:basis-0">
-                <div class="p-6">
-                    <a href="#!">
-                        <i class="fa-solid fa-file-invoice-dollar fa-2xl rounded-t-lg mt-6" style="font-size: 60px;"></i>
-                    </a>
-                    <h5 class="mb-2 mt-6 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
-                        Card title
-                    </h5>
-                    <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-                        This is a longer card with supporting text below as a natural
-                        lead-in to additional content. This content is a little bit
-                        longer.
-                    </p>
-                </div>
+            <div class="mx-12 mt-6 flex flex-col self-start rounded-lg bg-white rounded-lg shadow-2xl dark:bg-neutral-700 sm:shrink-0 sm:grow sm:basis-0">
+                <a href="#!">
+                    <div class="p-6">
+                        <i class="fa-solid fa-file-invoice-dollar fa-2xl rounded-t-lg mt-6 text-cyan-500" style="font-size: 60px;"></i>
+                        <h5 class="mb-2 mt-6 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+                            Invoices & Receipts
+                        </h5>
+                        <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+                            Here you can upload, store, view, and manage your invoices and receipts. 
+                        </p>
+                    </div>
+                </a>
             </div>
-            <div class="mx-12 mt-6 mb-12 flex flex-col self-start rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 sm:shrink-0 sm:grow sm:basis-0">
-                <div class="p-6">
-                    <a href="#!">
-                        <i class="fa-solid fa-file-invoice-dollar fa-2xl rounded-t-lg mt-6" style="font-size: 60px;"></i>
-                    </a>
-                    <h5 class="mb-2 mt-6 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
-                        Card title
-                    </h5>
-                    <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-                        This is a longer card with supporting text below as a natural
-                        lead-in to additional content. This content is a little bit
-                        longer.
-                    </p>
-                </div>
+            <div class="mx-12 mt-6 mb-12 flex flex-col self-start rounded-lg bg-white rounded-lg shadow-2xl dark:bg-neutral-700 sm:shrink-0 sm:grow sm:basis-0">
+                <a href="#!">
+                    <div class="p-6">
+                        <i class="fa-solid fa-pen-to-square fa-2xl rounded-t-lg mt-6 text-cyan-500" style="font-size: 60px;""></i>
+                        <h5 class="mb-2 mt-6 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+                            Regulatory Certificates
+                        </h5>
+                        <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+                            Here you can upload, store, and view your required landlord certifications.
+                        </p>
+                    </div>
+                </a>
             </div>
-            <div class="mx-12 mt-6 mb-12 flex flex-col self-start rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 sm:shrink-0 sm:grow sm:basis-0">
+            <div class="mx-12 mt-6 mb-12 flex flex-col self-start rounded-lg bg-white rounded-lg shadow-2xl dark:bg-neutral-700 sm:shrink-0 sm:grow sm:basis-0">
+                <a href="#!">
                 <div class="p-6">
-                    <a href="#!">
-                        <i class="fa-solid fa-file-invoice-dollar fa-2xl rounded-t-lg mt-6" style="font-size: 60px;"></i>
-                    </a>
+                    <i class="fa-solid fa-file-signature fa-2xl rounded-t-lg mt-6 text-cyan-500" style="font-size: 60px;""></i>
                     <h5 class="mb-2 mt-6 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
-                        Card title
+                        Tenancy Agreements
                     </h5>
                     <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-                        This is a longer card with supporting text below as a natural
-                        lead-in to additional content. This content is a little bit
-                        longer.
+                        Here you can upload, store, and view your tenancy agreements.
                     </p>
                 </div>
+                </a>
             </div>
         </div>
-
-        
-
+        <div class="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
+            <div class="max-w-lg mx-auto">
+                <a href="/addresses" class="block text-center px-5 py-3 text-sm font-medium text-white bg-indigo-600 rounded-lg">
+                    Back to Addresses
+                </a>  
+            </div>
+        </div>
     </body>
 </x-layout.app>
